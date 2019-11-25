a461a15350da757c5e9691fe4c45334efaf4eb78
Nicolas Zhou <nicolaszhou28@gmail.com>
2019-11-25 17:35:10 +1300

    optimization && prop-types for Modal and related components

--- src/jsx/components/Modal/Button.jsx
+++ src/jsx/components/Modal/Button.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import classnames from "classnames";
 import ButtonGroup from "./ButtonGroup";
+import PropTypes from "prop-types";
 
 import "../../../scss/Button.scss";
 
 const Button = props => {
 	const isColored = props.colored ? "button-colored" : "button-white";
 
+	function onclick(e) {
+		props.onclick && props.onclick(e);
+	}
+
 	return (
-		<button className={classnames("button", isColored, props.className)} onClick={props.onclick}>
-			{props.title}
+		<button className={classnames("button", isColored, props.className)} onClick={onclick}>
+			{props.title || "Button"}
 		</button>
 	);
 };
@@ -17,3 +22,10 @@ const Button = props => {
 Button.Group = ButtonGroup;
 
 export default Button;
+
+Button.propTypes = {
+	className: PropTypes.string,
+	onclick: PropTypes.func,
+	colored: PropTypes.bool,
+	title: PropTypes.string
+};
